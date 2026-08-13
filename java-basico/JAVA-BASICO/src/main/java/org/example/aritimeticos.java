1dd85883a3094d813ba3e541b6c7d16f3faf0f9e
KywzzS <kayo.sampaio@ba.estudante.senai.br>
2026-08-12 21:23:52 -0300

    Add files via upload

---
+++ java-basico/JAVA-BASICO/src/main/java/org/example/aritimeticos.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class aritimeticos {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int a = 10;
+        int b = 2;
+
+        // Operadores aritméticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a -b;
+
+        // Exibindo resultados
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+
+    }
+}
