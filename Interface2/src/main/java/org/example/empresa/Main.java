9da06ac353d61dafe93254d293c73f962c0db438
KywzzS <kayo.sampaio@ba.estudante.senai.br>
2026-09-09 22:18:28 -0300

    Atualizar o Main.java

--- Interface2/src/main/java/org/example/empresa/Main.java
+++ Interface2/src/main/java/org/example/empresa/Main.java
@@ -2,7 +2,7 @@ package org.example.empresa;
 
 public class Main {
     public static void main(String[] args) {
-        Motoboy motoboy = new Motoboy("Axor", "303030", Sexo.MASCULINO, Setor.OPERACOES, 4000.0, "1010");
+        Motoboy motoboy = new Motoboy("Carlos", "303030", Sexo.MASCULINO, Setor.OPERACOES, 4000.0, "1010");
         Diretor diretor = new Diretor("Robert", "404040", Sexo.MASCULINO, Setor.OPERACOES, 4000.0);
 
         System.out.println(diretor.toString());
